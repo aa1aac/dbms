bf74e7f7cd9f803f8338571447732a79fc1306d3
aa1aac <35446762+aa1aac@users.noreply.github.com>
2024-04-08 17:30:01 -0400

    update task 8

--- task-8.sql
+++ task-8.sql
@@ -1,15 +1,13 @@
--- Update typeWeight for all rows
+-- Change the percentages of the categories for a course;
 UPDATE AssignmentType
 SET typeWeight = 
     CASE
       WHEN courseId = 1 THEN
         CASE 
-          WHEN assignmentType = 'H' THEN 25.00
+          WHEN assignmentType = 'HW' THEN 25.00
           WHEN assignmentType = 'Pr' THEN 25.00
-          WHEN assignmentType = 'T' THEN 45.00
+          WHEN assignmentType = 'Te' THEN 45.00
           WHEN assignmentType = 'Pa' THEN 5.00
         END
       ELSE typeWeight
     END;
-
-SELECT * FROM AssignmentType;
