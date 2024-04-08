c71aab644ed4265603e7d81b08d91ccd0aa5f067
aa1aac <35446762+aa1aac@users.noreply.github.com>
2024-04-08 16:44:17 -0400

    update the task 1

--- task-2.sql
+++ task-2.sql
@@ -28,6 +28,15 @@ CREATE TABLE Enrollment(
     courseId INTEGER REFERENCES Course(courseId)
 );
 
+-- create assignmentType
+CREATE TABLE AssignmentType (
+    courseId INTEGER NOT NULL,
+    assignmentType VARCHAR(2),
+    typeWeight FLOAT,
+    FOREIGN KEY (courseId) REFERENCES Course(courseId),
+    PRIMARY KEY (courseId, assignmentType)
+);
+
 -- create assignment
 CREATE TABLE Assignment (
     assignmentId SERIAL PRIMARY KEY,
@@ -35,17 +44,8 @@ CREATE TABLE Assignment (
     assignmentName VARCHAR(100),
     assignmentDescription TEXT,
     assignmentPoint FLOAT, -- highest possible point
-    assignmentType VARCHAR(2) REFERENCES AssignmentType(assignmentType),
-    assignmentWeight FLOAT -- out of 100
-);
-
--- create assignmentType
-CREATE TABLE AssignmentType (
-    courseId INTEGER,
     assignmentType VARCHAR(2),
-    typeWeight FLOAT,
-    FOREIGN KEY (courseId) REFERENCES Course(courseId),
-    PRIMARY KEY (courseId, assignmentType)
+    FOREIGN KEY (courseId, assignmentType) REFERENCES AssignmentType(courseId, assignmentType)
 );
 
 -- create assignmentGrade
@@ -59,27 +59,53 @@ CREATE TABLE AssignmentGrade (
 -- Inserting values now
 -- Insert into Student Table
 INSERT INTO Student (studentFirstName, studentLastName, studentDateOfBirth)
-VALUES ('Jon', 'Doe', '2011-12-12');
+VALUES 
+('Jon', 'Doe', '2011-12-12'),
+('Robert', 'Beasley', '1999-06-2'),
+('Ramona', 'Miller', '2000-12-1'),
+('Lourdes', 'Lee', '02-04-2002'),
+('Carl', 'Fortney', '09-20-2001')
+;
 
 --Insert course table
 INSERT INTO Course (departmentCode, courseNumber, courseName, courseSemester, courseYear)
-VALUES ('CSCI', '401', 'Technical Elective', 'SP', '2024');
+VALUES 
+('CSCI', '402', 'Mobile App Development', 'SP', '2024'),
+('CSCI', '432', 'Database System', 'SP', '2024'),
+('CSCI', '449', 'Intro to Computer Networks', 'SP', '2024'),
+('CSCI', '453', 'Intro to Cyber Security I', 'SP', '2024');
 
 --Insert into enrollment table
 INSERT INTO Enrollment(studentId, courseId)
-VALUES (1, 1);
-
---Insert into assignment
-INSERT INTO Assignment(courseId, assignmentName, assignmentDescription, assignmentPoint, assignmentType, assignmentWeight)
-VALUES(1, 'Mid term', 'Mid term exam on chapter 1 to 5', 100, 'exam', 30);
-
--- Insert into assignmentGrade
-INSERT INTO AssignmentGrade(assignmentId, studentId, grade)
-VALUES(1, 1, 98);
+VALUES 
+    (1, 1),
+    (1, 2),
+    (1, 3),
+    (1, 4),
+    (2, 1),
+    (3, 1),
+    (4,1),
+    (5,1);
 
 -- Insert into assignmentType
 INSERT INTO AssignmentType(courseId, assignmentType, typeWeight)
-VALUES(1, 'H', 20.00),
+VALUES(1, 'Pa', 10.00),
+    (1, 'HW', 20.00),
+    (1, 'Te', 50.00),
     (1, 'Pr', 20.00),
-    (1, 'T', 50.00),
-    (1, 'Pa', 10.00);
+    (2, 'Pa', 10.00),
+    (2, 'HW', 20.00),
+    (2, 'Te', 50.00),
+    (2, 'Pr', 20.00);
+
+--Insert into assignment
+INSERT INTO Assignment(courseId, assignmentName, assignmentDescription, assignmentPoint, assignmentType)
+VALUES
+(1, 'Mid term', 'Mid term exam on chapter 1 to 5', 100, 'Te'),
+(2, 'Mid term', 'Mid term exam', 100, 'Te');
+
+-- Insert into assignmentGrade
+INSERT INTO AssignmentGrade(assignmentId, studentId, grade)
+VALUES
+(7, 1, 98),
+(8, 1, 97);
