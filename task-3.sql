e506dd999a10accb3be2e7aeb310ee8001915ef0
aa1aac <35446762+aa1aac@users.noreply.github.com>
2024-04-07 20:00:32 -0400

    task 3

---
+++ task-3.sql
@@ -0,0 +1,14 @@
+-- display student table
+SELECT * FROM Student;
+
+-- display Course table
+SELECT * FROM Course;
+
+-- display enrollment table
+SELECT * FROM Enrollment;
+
+-- display assignment table
+SELECT * FROM Assignment;
+
+-- display assignment grade table
+SELECT * FROM AssignmentGrade;
