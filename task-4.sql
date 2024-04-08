89b80e4749d70091fc99c7ef6c65d479e23e10ed
aa1aac <35446762+aa1aac@users.noreply.github.com>
2024-04-07 20:12:10 -0400

    task 4

---
+++ task-4.sql
@@ -0,0 +1,5 @@
+-- Compute the average/highest/lowest score of an assignment;
+SELECT AVG(AG.grade) AS Average, MAX(AG.grade) AS highest, MIN(AG.grade) AS Lowest
+FROM AssignmentGrade AS AG
+WHERE AG.assignmentId = 1
+GROUP BY AG.grade;
