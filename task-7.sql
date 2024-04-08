a6471c4aed9422f713e97875c5649e397183b9d2
aa1aac <35446762+aa1aac@users.noreply.github.com>
2024-04-07 21:04:58 -0400

    task 7

---
+++ task-7.sql
@@ -0,0 +1,3 @@
+-- Add an assignment to a course;
+INSERT INTO Assignment(courseId, assignmentName, assignmentDescription, assignmentPoint, assignmentType, assignmentWeight)
+VALUES(1, 'Final Exam', 'Final Exam', 100, 'exam', 10);
