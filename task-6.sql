64b034f3f004cb06008d327c42d9a8ded4f2ca2f
aa1aac <35446762+aa1aac@users.noreply.github.com>
2024-04-07 20:59:25 -0400

    task 6 edit

--- task-6.sql
+++ task-6.sql
@@ -1,5 +1,6 @@
 -- List all of the students in a course and all of their scores on every assignment;
-SELECT * 
+SELECT Student.studentId, Student.studentFirstName, Student.studentLastName, Student.studentDateOfBirth, AssignmentGrade.grade
 FROM Student
-INNER JOIN assignmentGrade ON assignmentGrade.studentId = Student.studentId
-WHERE assignmentGrade.assignmentGrade = 1;
+INNER JOIN Enrollment ON Enrollment.studentId=Student.studentId
+INNER JOIN AssignmentGrade ON AssignmentGrade.studentId = Student.studentId
+WHERE Enrollment.courseId=1;
