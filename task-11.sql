b4ad999b6c0ef6c4ab76e4973637537c4be93fec
aa1aac <35446762+aa1aac@users.noreply.github.com>
2024-04-08 20:56:01 -0400

    task 11

---
+++ task-11.sql
@@ -0,0 +1,19 @@
+SELECT
+    student.studentId,
+    student.studentFirstName,
+    student.studentLastName,
+    assignment.courseId,
+    SUM(assignmentGrade.grade / assignment.assignmentPoint * assignmentType.typeWeight) AS overallGrade
+FROM
+    Student student
+JOIN
+    AssignmentGrade assignmentGrade ON student.studentId = assignmentGrade.studentId
+JOIN
+    Assignment assignment ON assignmentGrade.assignmentId = assignment.assignmentId
+JOIN
+    AssignmentType assignmentType ON assignment.courseId = assignmentType.courseId AND assignment.assignmentType = assignmentType.assignmentType
+GROUP BY
+    student.studentId,
+    student.studentFirstName,
+    student.studentLastName,
+    assignment.courseId;
