191675af31da76575b3d209bf380dfb05da89d9c
aa1aac <35446762+aa1aac@users.noreply.github.com>
2024-04-09 20:42:26 -0400

    update

--- task-12.sql
+++ task-12.sql
@@ -1,26 +1,42 @@
--- -- Compute the grade for a student, where the lowest score for a given category is dropped.
-WITH LowestGrades AS (
+WITH RankedGrades AS (
     SELECT
-        studentId,
+        ag.studentId,
         a.courseId,
-        a.assignmentType,
-        MIN(grade) AS lowestGrade
+        at.assignmentType,
+        ag.grade,
+        COUNT(*) OVER (PARTITION BY ag.studentId, a.courseId, at.assignmentType) AS numAssignments,
+        ROW_NUMBER() OVER (PARTITION BY ag.studentId, a.courseId, at.assignmentType ORDER BY ag.grade ASC) AS rank
     FROM
         AssignmentGrade ag
     JOIN
         Assignment a ON ag.assignmentId = a.assignmentId
     JOIN
         AssignmentType at ON a.courseId = at.courseId AND a.assignmentType = at.assignmentType
-    GROUP BY
-        studentId, a.courseId, a.assignmentType
-),
-TypesCount AS (
-    SELECT
-    courseId,
-    assignmentType,
-    COUNT(*) AS numAssignments
-    FROM
-        Assignment
-    GROUP BY
-        courseId, assignmentType
 )
+SELECT
+    s.studentId,
+    s.studentFirstName,
+    s.studentLastName,
+    rg.courseId,
+    SUM(
+        CASE
+            WHEN rg.numAssignments = 1 THEN rg.grade / a.assignmentPoint * at.typeWeight
+            WHEN rg.rank > 1 THEN rg.grade / a.assignmentPoint * at.typeWeight / (rg.numAssignments - 1)
+            ELSE 0
+        END
+    ) AS overallGrade
+FROM
+    Student s
+JOIN
+    RankedGrades rg ON s.studentId = rg.studentId
+JOIN
+    Assignment a ON rg.courseId = a.courseId
+JOIN
+    AssignmentType at ON a.courseId = at.courseId AND a.assignmentType = at.assignmentType
+WHERE 
+    s.studentId = 1
+GROUP BY
+    s.studentId,
+    s.studentFirstName,
+    s.studentLastName,
+    rg.courseId;
